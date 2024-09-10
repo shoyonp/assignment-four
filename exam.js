eb79ef261f5e47e90a2ef6fd800fe18b9104fc81
shoyonp <shoyonkumar143@gmail.com>
2024-09-10 20:28:45 +0600

    problem one done

--- exam.js
+++ exam.js
@@ -1,6 +1,16 @@
 // porblem 1
 
 function calculateTax(income, expenses) {
+
+    if(expenses > income || income < 0 || expenses < 0){
+        return  "Invalid Input"
+    }
+
+        const diff = income - expenses;
+        const tax = diff * .20;
+        return tax;
     
 }
 
+const result = calculateTax(34000, 1753);
+console.log(result);
